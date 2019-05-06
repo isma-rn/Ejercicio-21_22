5f6396d9172b8ac711d1617e2636327b22569818
Juan Ismael Rosas Nava <JIsmael-RN@outlook.com>
2019-05-05 22:43:15 -0500

    Inicio

---
+++ E21_E22_DeterminarMayorMenorEnArreglo/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace E21_E22_DeterminarMayorMenorEnArreglo
+{   
+    //21.- dado un arreglo Arr de tipo entero.Determinar el número más grande almacenado
+    //22.- determinar el minimo del anterior
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            int[] Arreglo = ObtenerVectorConEnterosAleatorios( 15, 10, 99);
+            Console.WriteLine("El arreglo es:");
+            foreach (var v in Arreglo)
+                Console.WriteLine("[{0}] ", v);
+            Console.WriteLine("El valor mayor es: {0}", ObtenerMayor(Arreglo));
+            Console.WriteLine("El valor menor es: {0}", ObtenerMenor(Arreglo));
+            Console.ReadLine();
+        }
+
+        public static int ObtenerMayor(int[] array)
+        {
+            int mayor = 0;
+            if (array.Length > 0)            
+                foreach (var v in array)
+                    if(v > mayor)                    
+                        mayor = v;
+            return mayor;
+        }
+
+        public static int ObtenerMenor(int[] array)
+        {
+            int menor = 0;
+            if (array.Length > 0)
+            {
+                menor = array[0];
+                foreach (var v in array)
+                    if (v < menor)
+                        menor = v;
+            }
+                
+            return menor;
+        }
+
+        public static int[] ObtenerVectorConEnterosAleatorios(int size, int min, int max)
+        {
+            Random random = new Random();
+            int[] resultado = null;
+            if (size > 0 && min < max)
+            {
+                resultado = new int[size];
+                for (int i = 0; i < resultado.Length; i++)
+                    resultado[i] = random.Next(min, max);
+            }
+
+            return resultado;
+        }
+    }
+}
